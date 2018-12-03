d32592bbe1a1cd82fedd9c9ef0fdc11b59944390
Stephen Christensen <stephen.m.christensen@aexp.com>
2018-12-03 18:52:33 -0500

    Lab 13 complete

---
+++ if-test/if-hostname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#Prompt for the hostname
+hostname = input('Please enter the hostname: ').upper()
+
+# Test to see if hostname matches expected response
+if hostname == 'MTG':
+  print('The hostname was found to be mtg')
+  print('Hostname matches expexted config')
+print('Exiting script!')
