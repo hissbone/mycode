a6aa210eddc3f9cf96dafa819faf1ae308a6abb8
Stephen Christensen <stephen.m.christensen@aexp.com>
2018-12-04 15:44:58 -0500

    Lab 14 complete

---
+++ if-test2/iftest2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env pyton3
+
+ipchk = input('please enter an IP address: ')
+
+if ipchk:
+    print('It looks like IP address was set: ' + ipchk)
+else:
+    print('You did not supply any input!')
