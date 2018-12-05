fa0457e442d19e96766230117a82cc4dc098ae97
Stephen Christensen <stephen.m.christensen@aexp.com>
2018-12-05 18:04:40 -0500

    Lab 26 complete

---
+++ smallscript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+Author: SChristensen
+
+This script print things to the screen
+
+'''
+
+def main():
+    '''Our main function'''
+    mynote = 'print this string'
+    print(mynote)
+
+main()
